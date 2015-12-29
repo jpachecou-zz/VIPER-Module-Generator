7e1f9154cc7871d906d6cd346508865b41287388
Jonathan Pacheco <jonathan261090@gmail.com>
2015-12-29 16:36:06 -0500

    Initial Commit

---
+++ ViperModules/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ViperModules
+//
+//  Created by Jonathan Pacheco on 29/12/15.
+//  Copyright © 2015 Jonathan Pacheco. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ViperModules/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ViperModules
+//
+//  Created by Jonathan Pacheco on 29/12/15.
+//  Copyright © 2015 Jonathan Pacheco. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
